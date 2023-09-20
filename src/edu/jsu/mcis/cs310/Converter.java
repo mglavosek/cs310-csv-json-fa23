6c671ca84e972fe5f94af60ff0cdbb7fd06b4cb7
Matthew Glavosek <student49841@gmail.com>
2023-09-20 12:59:01 -0500

    Completed Converter.java

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -2,6 +2,8 @@ package edu.jsu.mcis.cs310;
 
 import com.github.cliftonlabs.json_simple.*;
 import com.opencsv.*;
+import java.io.*;
+import java.util.*;
 
 public class Converter {
     
@@ -74,36 +76,100 @@ public class Converter {
     @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
         
-        String result = "{}"; // default return value; replace later!
-        
+        String result = "";
+                      
         try {
+        // Create JSON objects
+            JsonObject jsonObject = new JsonObject();
+            JsonArray prodNumsArray = new JsonArray();
+            JsonArray colHeadingsArray = new JsonArray();
+            JsonArray dataArray = new JsonArray();
+
+            // Create a CSV reader
+            CSVReader csvReader = new CSVReader(new StringReader(csvString));
+            List<String[]> csvRows = csvReader.readAll();
+
+            // Get headers from the first row
+            String[] headers = csvRows.get(0);
+
+            for (String header : headers) {
+                colHeadingsArray.add(header);
+            }
+
+            // Loop through the rows and convert them to JSON arrays
+            for (int i = 1; i < csvRows.size(); i++) {
+                String[] row = csvRows.get(i);
+                JsonArray rowDataArray = new JsonArray();
+
+                for (String cell : row) {
+                    rowDataArray.add(cell);
+                }
+
+                dataArray.add(rowDataArray);
+            }
+
+            jsonObject.put("ProdNums", prodNumsArray);
+            jsonObject.put("ColHeadings", colHeadingsArray);
+            jsonObject.put("Data", dataArray);
+
+            // Serialize the JSON object to a string
+            return jsonObject.toJson();
         
-            // INSERT YOUR CODE HERE
-            
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         
-        return result.trim();
-        
+        return result;
     }
     
     @SuppressWarnings("unchecked")
     public static String jsonToCsv(String jsonString) {
         
-        String result = ""; // default return value; replace later!
+        var result = "";
         
         try {
-            
-            // INSERT YOUR CODE HERE
-            
+        
+            // Create a JSON object from the input string
+            JsonObject jsonObject = (JsonObject) Jsoner.deserialize(jsonString);
+
+            // Extract column headings
+            JsonArray colHeadingsArray = (JsonArray) jsonObject.get("ColHeadings");
+            String[] headers = new String[colHeadingsArray.size()];
+            for (int i = 0; i < colHeadingsArray.size(); i++) {
+                headers[i] = (String) colHeadingsArray.get(i);
+            }
+
+            // Initialize a CSV writer
+            StringWriter stringWriter = new StringWriter();
+            CSVWriter csvWriter = new CSVWriter(stringWriter);
+
+            // Write the headers to the CSV
+            csvWriter.writeNext(headers);
+
+            // Extract data rows and write them to the CSV
+            JsonArray dataArray = (JsonArray) jsonObject.get("Data");
+            for (Object row : dataArray) {
+                JsonArray rowDataArray = (JsonArray) row;
+                String[] csvRow = new String[rowDataArray.size()];
+                for (int i = 0; i < rowDataArray.size(); i++) {
+                    csvRow[i] = rowDataArray.get(i).toString(); 
+                }
+                csvWriter.writeNext(csvRow);
+            }
+
+            // Close the CSV writer
+            csvWriter.close();
+
+            // Get the CSV content from the StringWriter
+            return stringWriter.toString(); 
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         
-        return result.trim();
+        
+        return result;
         
     }
     
